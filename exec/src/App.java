f4885be71e5daf7fea54710aa63356c997c8300f
3oliveir <3oliveir@informatik.uni-hamburg.de>
2020-09-25 22:58:56 +0200

    upload e exercicio 1

---
+++ exec/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       MaiorNumero mn = new MaiorNumero();
+       System.out.println(mn.maior(5, 8));
+    }
+}
---
+++ exec/src/MaiorNumero.java
@@ -0,0 +1,5 @@
+public class MaiorNumero {
+    public int maior(int a, int b){
+        return a > b ? a : b;
+    }
+}
